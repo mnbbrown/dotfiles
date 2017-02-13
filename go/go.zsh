2d7fdf974ee8eff049a73e5341f15a4a12b4cea4
Matthew Brown <me@matthewbrown.io>
2017-02-14 07:52:06 +1000

    Removed local go config

--- go/go.zsh
+++
@@ -1,6 +0,0 @@
-export GOVERSION=$(brew list go --versions | awk '{print $2}')
-export GOPATH=$PROJECTS/go
-export GOROOT=$(brew --prefix)/Cellar/go/$GOVERSION/libexec
-export GOOS=darwin
-export PATH=$GOPATH/bin:$PATH
-export GO15VENDOREXPERIMENT=1
