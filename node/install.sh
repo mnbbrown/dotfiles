7386a887be243f66fff1ecb4fd1a7fae384a5a7e
Matthew Brown <matt.brown@bcgdv.com>
2021-04-26 08:07:57 +0100

    fix tmux config. add some more apps to Brewfile. udpate node/install

--- node/install.sh
+++ node/install.sh
@@ -2,7 +2,10 @@
 set -e
 
 echo "  Installing node for you."
-mkdir -p ~/.nvm
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+export NVM_DIR="$HOME/.nvm" && (
+  git clone https://github.com/nvm-sh/nvm.git "$NVM_DIR"
+  cd "$NVM_DIR"
+  git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`
+) && \. "$NVM_DIR/nvm.sh"
 nvm install node
 npm install -g yarn 2>&1 | tee /tmp/node-install.log
